3056876a27331d624ba8d19399bc89d08eb31473
lordigquest <32837857+lordigquest@users.noreply.github.com>
2019-01-29 16:32:35 +0100

    added new changing color technologie

---
+++ BauhausRacer/Assets/Prefabs/lordigs car/ColorChangeTestsript.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChangeTestsript : MonoBehaviour {
+    bool inTransition = false;
+    int toggle = 1;
+    public Material material;
+    public float changespeed = 1;
+    public float changevalue = 1.5f;
+    Color curerntColor;
+    public Color newColor;
+    public Color startColor;
+
+	// Use this for initialization
+	void Start () {
+        curerntColor = startColor;
+        material.SetColor("Color_ABE336C8",curerntColor);
+        material.SetColor("Color_6066237A",curerntColor);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (inTransition)
+        {
+            ExperimentalColorChange();
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                PrepareTransition();
+            }
+        }
+    }
+
+    public void PrepareTransition(Color color)
+    {
+        if (toggle == 1)
+        {
+            material.SetColor("_Color_Transition",color);
+        }
+        else
+        {
+            material.SetColor("_Color_Surface",color);
+        }
+
+        inTransition = true;
+        toggle *= -1;
+    }
+
+    void PrepareTransition()
+    {
+        if (toggle == 1)
+        {
+            material.SetColor("_Color_Transition",newColor);
+        }
+        else
+        {
+            material.SetColor("_Color_Surface",newColor);
+        }
+        
+        inTransition = true;
+        toggle *= -1;
+    }
+
+    void ExperimentalColorChange()
+    {
+        changevalue = changevalue + changespeed * toggle * Time.deltaTime;
+        material.SetFloat("_Vector1_Transition",changevalue);
+        
+        if((changevalue > 1.5 && toggle == 1) || (changevalue < -0.5 && toggle == -1))
+        {
+            inTransition = false;
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        curerntColor = startColor;
+        material.SetColor("_Color_Surface",curerntColor);
+        material.SetColor("_Color_Transition",curerntColor);
+    }
+}
---
+++ BauhausRacer/Assets/Scripts/Car/ChangeColorByShader.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColorByShader : MonoBehaviour {
+
+    bool inTransition = false;
+    int toggle = 1;
+    public Material material;
+    public float changespeed = 1;
+    private float changevalue = 1.5f;
+    Color curerntColor;
+    public Color startColor;
+
+    // Use this for initialization
+    void Start()
+    {
+        curerntColor = startColor;
+        material.SetColor("_Color_Transition",curerntColor);
+        material.SetColor("_Color_Surface",curerntColor);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inTransition)
+        {
+            ExperimentalColorChange();
+        }
+    }
+
+    public void PrepareTransition(Color color)
+    {
+        if (toggle == 1)
+        {
+            material.SetColor("_Color_Transition",color);
+        }
+        else
+        {
+            material.SetColor("_Color_Surface",color);
+        }
+
+        inTransition = true;
+        toggle *= -1;
+    }
+
+    void ExperimentalColorChange()
+    {
+        changevalue = changevalue + changespeed * toggle * Time.deltaTime;
+        material.SetFloat("_Vector1_Transition",changevalue);
+
+        if (( changevalue > 1.5 && toggle == 1 ) || ( changevalue < -0.5 && toggle == -1 ))
+        {
+            inTransition = false;
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        curerntColor = startColor;
+        material.SetColor("_Color_Transition",curerntColor);
+        material.SetColor("_Color_Surface",curerntColor);
+    }
+}
