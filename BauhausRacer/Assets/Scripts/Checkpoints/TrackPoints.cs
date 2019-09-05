955bd1a2bfa63fb3f5a70466f3f2d2f3cbeba63c
lordigquest <philip.ziegler91@gmail.com>
2019-09-05 12:19:14 +0200

    streckenpunkte hinzugefügt

---
+++ BauhausRacer/Assets/Scripts/Checkpoints/TrackPoints.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BauhausRacer
+{
+    public class TrackPoints : MonoBehaviour
+    {
+        [SerializeField] TrackPoints[] nextTrackpoints;
+        // Use this for initialization
+        void Start()
+        {
+            CheckMissingLinks();
+        }
+
+        void CheckMissingLinks()
+        {
+            if (nextTrackpoints == null || nextTrackpoints.Length == 0)
+            {
+                PrintErrorLog();
+                return;
+            }
+            foreach(TrackPoints tp in nextTrackpoints)
+            {
+                try
+                {
+                    if (tp == null)
+                    {
+                        PrintErrorLog();
+                        return;
+                    }
+                }
+                catch
+                {
+                    PrintErrorLog();
+                }
+                
+            }
+        }
+
+        void PrintErrorLog()
+        {
+            Debug.LogError(string.Format("{0} hat keinen oder fehlende nächste Punkte!",transform.name));
+        }
+    }
+}
